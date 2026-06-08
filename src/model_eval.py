6937804f56cd3d512fb4212e2c0dfe16d162ee1c
Sebin Sunny <100014504+sebin07r@users.noreply.github.com>
2026-06-08 09:55:20 +0530

    Update model_eval.py

--- src/model_eval.py
+++ src/model_eval.py
@@ -1,10 +1,18 @@
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import (
+    mean_squared_error,
+    mean_absolute_error,
+    r2_score
+)
 import numpy as np
 
 def evaluate_model(y_true, y_pred):
 
     rmse = np.sqrt(mean_squared_error(y_true, y_pred))
+    mae = mean_absolute_error(y_true, y_pred)
+    r2 = r2_score(y_true, y_pred)
 
-    print("RMSE:", rmse)
+    print(f"RMSE : {rmse:.4f}")
+    print(f"MAE  : {mae:.4f}")
+    print(f"R²   : {r2:.4f}")
 
-    return rmse
+    return rmse, mae, r2
