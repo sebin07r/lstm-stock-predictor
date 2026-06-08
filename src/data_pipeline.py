fc096144dabab0f34119539e3ef1f07eb9c2a7bf
Sebin Sunny <100014504+sebin07r@users.noreply.github.com>
2026-06-08 09:57:34 +0530

    Update data_pipeline.py

--- src/data_pipeline.py
+++ src/data_pipeline.py
@@ -1,11 +1,3 @@
-import yfinance as yf
-import numpy as np
-from sklearn.preprocessing import MinMaxScaler
-
-def load_stock_data(ticker="AAPL"):
-    data = yf.download(ticker, start="2015-01-01", end="2024-01-01")
-    return data['Close'].values.reshape(-1,1)
-
 def preprocess_data(data, sequence_length=60):
     scaler = MinMaxScaler(feature_range=(0,1))
     scaled = scaler.fit_transform(data)
@@ -16,4 +8,15 @@ def preprocess_data(data, sequence_length=60):
         X.append(scaled[i-sequence_length:i])
         y.append(scaled[i])
 
-    return np.array(X), np.array(y), scaler
+    X = np.array(X)
+    y = np.array(y)
+
+    split = int(len(X) * 0.8)
+
+    X_train = X[:split]
+    X_test = X[split:]
+
+    y_train = y[:split]
+    y_test = y[split:]
+
+    return X_train, X_test, y_train, y_test, scaler
